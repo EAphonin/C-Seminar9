12c42adfdcdd9ab0235d7bd62af5d079307b9452
evgenyi <fonya93@yandex.ru>
2023-05-30 11:16:18 +0700

    Сумма от M до N

---
+++ task66/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int m = GetPositivUserNumber("Введите начальное значение M: ", "Ошибка ввода");
+int n = GetPositivUserNumber("Введите конечное значение N: ", "Ошибка ввода");
+
+if (n > m)
+{
+    int result = GetSumNumber(m, n);
+    Console.WriteLine($" M = {m}; N = {n} -> {result}");
+}
+else
+{
+    Console.WriteLine("Должен быть задан промежуток, введите корректное значение N ");
+}
+
+int GetSumNumber(int m, int n)
+{
+    if (m > n) return 0;
+    else return m + GetSumNumber(m + 1, n);
+}
+
+int GetPositivUserNumber(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber) && userNumber >= 0)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
